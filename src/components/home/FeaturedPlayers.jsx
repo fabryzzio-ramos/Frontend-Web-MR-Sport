6da2a002623ce74e191ec3ad06fdfe2d29224adc
fabryzzio-ramos <fabryzzio@gnail.com>
2025-12-26 13:44:07 -0500

    actualizado

--- src/components/home/FeaturedPlayers.jsx
+++ src/components/home/FeaturedPlayers.jsx
@@ -18,6 +18,8 @@ function FeaturedPlayers() {
             cargarJugadores();
         }, []);
 
+    const destacados = jugadores.splice(0, 6);
+
     return (
         <section className="bg-gradient-to-b from-slate-900 to-[#020617] py-20">
             <div className="max-w-7xl mx-auto px-6">
@@ -28,13 +30,16 @@ function FeaturedPlayers() {
                     <h2 className="text-4xl font-extrabold text-white mt-2">Orgullo del Club</h2>
                 </div>
 
-                {/* GRID */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-                    {jugadores.map((jugador) => (
-                        <PlayerCard key={jugador._id} jugador={jugador} />
-                    ))}
-                </div>
-
+                {destacados.length ===  0 ? (
+                    <p className="text-center text-gray-400">Aún no hay jugadores</p>
+                ) : (
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
+                        {jugadores.map((jugador) => (
+                            <PlayerCard key={jugador._id} jugador={jugador} />
+                        ))}
+                    </div>
+                )}
+                
                 <div className="mt-12 flex justify-center">
                     <button className="px-8 py-3 border border-red-500 text-red-500 rounded-full hover:bg-red-500 hover:text-white font-semibold transition">Ver equipo completo</button>
                 </div>
