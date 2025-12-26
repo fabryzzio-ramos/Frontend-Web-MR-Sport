6b8ad8ccfd80c7074f30fbd4f616e821863f475f
fabryzzio-ramos <fabryzzio@gnail.com>
2025-12-26 13:40:37 -0500

    actualizado

--- src/components/home/NextMatch.jsx
+++ src/components/home/NextMatch.jsx
@@ -24,23 +24,27 @@ function NextMatch() {
         <section className="bg-gradient-to-b from-black to-slate-900 px-10 py-20 ">
             <div className="max-w-7xl mx-auto px-6 md:px-10 lg:px-16">
                 <SectionTitle title="Proximo Partido" subtitle="Calendario oficial del club" />
+
+                {partidos.length === 0 ? (
+                    <p className="text-center text-gray-400 mt-10">No hay partidos programados</p>
+                ) : (
+                    
+                    <div className="grid gap-8
+                    grid-cols-1
+                    sm:grid-cols-2
+                    md:grid-cols-3
+                    lg:grid-cols-4 mt-12">
+                        {partidos.map((partido) => (
+                        <MatchCard key={partido._id} partido={partido} />
+                        ))}
+                    </div>
+                )}
+
+                <div className="mt-14 flex justify-center">
+                    <Link to="/partidos" className="px-8 py-3 border border-red-500 text-red-500 rounded-full hover:bg-red-500 hover:text-white font-semibold transition">Ver calendario completo</Link>
+                </div>
             </div>
 
-            {/* GRID */}
-            <div className="grid gap-8
-            grid-cols-1
-            sm:grid-cols-2
-            md:grid-cols-3
-            xl:grid-cols-4">
-                {partidos.map((partido) => (
-                    <MatchCard key={partido._id} partido={partido} />
-                ))}
-            </div>
-
-            <div className="mt-12 flex justify-center">
-                <button className="px-8 py-3 border border-red-500 text-red-500 rounded-full hover:bg-red-500 hover:text-white font-semibold transition">Ver calendario completo</button>
-            </div>
-            
         </section>
     );
 }
