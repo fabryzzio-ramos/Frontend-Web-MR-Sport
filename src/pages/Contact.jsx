ea3f84185ca09874e34f0b1cf207c1a7b75c3518
fabryzzio-ramos <fabryzzio@gnail.com>
2025-12-26 22:32:25 -0500

    actualizado

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,3 @@
-import { Helmet } from "react-helmet-async";
 import ContactIntro from "../components/contact/ContactIntro";
 import ContactInfo from "../components/contact/ContactInfo";
 import ContactForm from "../components/contact/ContactForm";
@@ -6,12 +5,6 @@ import ContactForm from "../components/contact/ContactForm";
 function Contact() {
     return (
         <>
-            <Helmet>
-                <title>MR Sport | Club Deportivo Oficial</title>
-                <meta name="descripcion" content="MR Sport es un club deportivo basado en pasión, disciplina y familia.
-                Conoce el equipo, partidos y tienda oficial." 
-                />
-            </Helmet>
             <ContactIntro />
             <ContactInfo />
             <ContactForm />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import { Helmet } from "react-helmet-async";
 import Hero from "../components/home/Hero";
 import AboutClub from "../components/home/AboutClub";
 import NextMatch from "../components/home/NextMatch";
@@ -9,13 +8,6 @@ import Contact from "../components/home/Contact";
 function Home() {
     return (
         <>
-            <Helmet>
-                <title>MR Sport | Club Deportivo Oficial</title>
-                <meta name="descripcion" content="MR Sport es un club deportivo basado en pasión, disciplina y familia.
-                Conoce el equipo, partidos y tienda oficial." 
-                />
-            </Helmet>
-
             <Hero />
             <NextMatch />
             <FeaturedPlayers />
