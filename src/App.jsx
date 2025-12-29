b664324b2756c6c67f525bd7a675064e591e729a
fabryzzio-ramos <fabryzzio@gnail.com>
2025-12-29 12:01:19 -0500

    actualizado

--- src/App.jsx
+++ src/App.jsx
@@ -42,8 +42,8 @@ function AppLayout() {
 
     return (
         <div className="min-h-screen bg-black text-white flex flex-col">
-            {!isAdminRoute && <NavBar />}
-            {!isStoreRoute && <NavBar /> || <Footer />}
+            {!isAdminRoute && !isStoreRoute && <NavBar />}
+            {!isStoreRoute && <Footer />}
 
             <main className="flex-1">
                 <Suspense fallback={<PageLoader />}>
