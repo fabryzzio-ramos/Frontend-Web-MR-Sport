681800eda6a8bb5adb773c9730328fb9caa3e556
fabryzzio-ramos <fabryzzio@gnail.com>
2025-12-29 14:41:29 -0500

    actualizado

--- src/pages/store/ProductCard.jsx
+++ src/pages/store/ProductCard.jsx
@@ -3,9 +3,15 @@ import { useCart } from "../../context/CartContext";
 function ProductCard({ producto}) {
     const { addToCart } = useCart();
 
+    const imageUrl = producto.imagen?.url 
+        ? producto.imagen.url.replace("/upload/", "/upload/w_400,h_400,c_fill,g_center,q_auto,f_auto/") 
+        : "/placeholder.png";
+
     return (
         <div className="bg-[#020617] border border-white/10 rounded-2xl overflow-hidden hover:border-red-500/40 transition group">
-            <img src={producto.imagen?.url} alt={producto.nombre} className="w-full h-full object-cover group-hover:scale-105 transition" />
+            <div className="h-64">
+                <img src={producto.imagen?.url} alt={producto.nombre} className="w-full h-full object-cover group-hover:scale-105 transition" />
+            </div>
 
             <div className="p-4 text-center">
                 <h3 className="text-white font-semibold text-sm">{producto.nombre}</h3>
